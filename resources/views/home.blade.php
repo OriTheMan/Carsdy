a450b17799bb3c4c3c98b2e862c868a7a26d6f30
TsimpDim <dimitristsimpitas@gmail.com>
2019-01-25 14:14:28 +0200

    Add header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,18 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+        <link rel="stylesheet" type="text/css" href={{ URL::asset('css/carsdy.css') }}>
         <title>Carsdy</title>
     </head>
+
     <body>
-        
+        <div id="header">
+            <img id="logo" src={{asset('img/carsdy.png')}}>
+
+            <a class="header_segment" id="create_card" href="/login">Create card</a>
+            <a class="header_segment" id="login" href="/login">Login</a>
+        </div>
     </body>
 </html>
