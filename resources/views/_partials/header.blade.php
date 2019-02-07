f6a279975c31fb6493c370d8c625ad0342b7a624
TsimpDim <dimitristsimpitas@gmail.com>
2019-02-07 10:17:43 +0200

    Add profile to header

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -4,16 +4,20 @@
     </a>
 
 
-    <a class="header_segment" id="create_card" href="/create_set">Create card</a>
+    <a class="header_segment" id="create_card" href="/create_set">Create cards</a>
 
     @auth
-        <a href="{{ route('logout') }}" class="header_segment" id="logout_but" onclick="event.preventDefault(); document.getElementById('frm-logout').submit();">
-        Logout
-        </a> 
+        <div id="segments_right">
+            <a class="header_segment" id="profile_but" href="/profile" style="self-align:flex-end;"><i class="fas fa-user"></i></a>
 
-        <form id="frm-logout" action="{{ route('logout') }}" method="POST" style="display: none;">
-            {{ csrf_field() }}
-        </form>
+            <a href="{{ route('logout') }}" class="header_segment" id="logout_but" onclick="event.preventDefault(); document.getElementById('frm-logout').submit();">
+            Logout
+            </a> 
+
+            <form id="frm-logout" action="{{ route('logout') }}" method="POST" style="display: none;">
+                {{ csrf_field() }}
+            </form>
+        </div>
     @endauth
 
     @guest
